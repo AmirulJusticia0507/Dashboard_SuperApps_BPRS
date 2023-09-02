7384879cbbb13101de27825754d4c69fb185472e
AmirulJusticia0507 <55829147+AmirulJusticia0507@users.noreply.github.com>
2023-09-02 17:58:49 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dashboard_SuperApps_BPRS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
